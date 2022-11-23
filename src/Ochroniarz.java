453e730c5a39ccbe6f7506105d03de7f84264ff4
MandS <monika.domaradzkaa@gmail.com>
2022-11-23 15:43:01 +0000

    Krotka ankieta na powitanie w klubie

--- src/Ochroniarz.java
+++ src/Ochroniarz.java
@@ -2,28 +2,40 @@ import java.util.Scanner;
 
 public class Ochroniarz {
     public static void main(String[] args) {
-        System.out.println("Podaj imie");
+        System.out.println("What's your name?");
         Scanner scanner=new Scanner(System.in);
         String name=scanner.nextLine();
-        System.out.println("Podaj nazwisko");
+        System.out.println("What's your surname?");
         String surname=scanner.nextLine();
-        System.out.println("Podaj miejsce zamieszkania");
+        System.out.println("Where do you live?");
         String city=scanner.nextLine();
-        System.out.println("Podaj wiek");
+        System.out.println("How old are you?");
         int age=scanner.nextInt();
-        System.out.println("Czy jestes w zwiazku? Wpisz true albo false");
-        boolean zwiazek=scanner.nextBoolean();
-        System.out.println("Witaj "+ name+" "+surname);
+        System.out.println("Are you in a relationship? Please enter, true if yes and false if no");
+        boolean relationship=scanner.nextBoolean();
 
-        System.out.println("Zamieszkaly w "+city);
-        System.out.println("lat"+age);
-        if (zwiazek==true){
-            System.out.println("W zwiazku");
+        System.out.println(name.toUpperCase());
+        System.out.println(surname.toUpperCase());
+        System.out.println(city.toUpperCase());
+        System.out.println(age);
+        if (relationship){
+            System.out.println("In relationship");
         }
         else{
-            System.out.println("Singiel");
+            System.out.println("Single");
         }
-        System.out.println("w klubie Testersi");
+        System.out.println("Please check if the above information is correct. Please enter true if yes and false if no ");
+        boolean information= scanner.nextBoolean();
+        if (information) {
+            System.out.println("Hello " + name + " " + surname + ", welcome in Tester club <3 ");
+        }
+        else{
+            System.out.println("Please amend your details");
+
+        }
+
+
+        }
+
 
     }
-}
